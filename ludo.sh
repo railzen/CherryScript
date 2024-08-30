302c6ad17b34e72ca6db0acefb75fa55643ae551
railzen <ssharpcc@gmail.com>
2024-08-31 02:52:54 +0800

    [CHG] Modify bash command

--- ludo.sh
+++ ludo.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #cp -f ./ludo.sh ${work_path}/ludo.sh > /dev/null 2>&1
 
-main_version="V1.0.9118 Build240830"
+main_version="V1.0.9120 Build240831"
 work_path="/opt/CherryScript"
 
 main_menu_start() {
