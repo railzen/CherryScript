3912206937f55af0f6dc3f048b5824dae511a01a
RailZen <railzen@126.com>
2025-02-23 13:11:51 +0800

    [CHG]FIX BUG

--- ludo.sh
+++ ludo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #cp -f ./ludo.sh ${work_path}/ludo.sh > /dev/null 2>&1
 
-main_version="V1.0.956 Build250222"
+main_version="V1.0.97 Build250222"
 work_path="/opt/CherryScript"
 
 main_menu_start() {
