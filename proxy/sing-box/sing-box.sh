bcd924751dc2314327d41256b2101861f1a27fed
RailZen <railzen@126.com>
2025-04-01 23:57:46 +0800

    [CHG] Add singbox

---
+++ proxy/sing-box/sing-box.sh
@@ -0,0 +1,377 @@
+#!/bin/bash
+
+author=railzen
+
+# bash fonts colors
+red='\e[31m'
+yellow='\e[33m'
+gray='\e[90m'
+green='\e[92m'
+blue='\e[94m'
+magenta='\e[95m'
+cyan='\e[96m'
+none='\e[0m'
+_red() { echo -e ${red}$@${none}; }
+_blue() { echo -e ${blue}$@${none}; }
+_cyan() { echo -e ${cyan}$@${none}; }
+_green() { echo -e ${green}$@${none}; }
+_yellow() { echo -e ${yellow}$@${none}; }
+_magenta() { echo -e ${magenta}$@${none}; }
+_red_bg() { echo -e "\e[41m$@${none}"; }
+
+is_err=$(_red_bg 错误!)
+is_warn=$(_red_bg 警告!)
+
+err() {
+    echo -e "\n$is_err $@\n" && exit 1
+}
+
+warn() {
+    echo -e "\n$is_warn $@\n"
+}
+
+# root
+[[ $EUID != 0 ]] && err "当前非 ${yellow}ROOT用户.${none}"
+
+# yum or apt-get, ubuntu/debian/centos
+cmd=$(type -P apt-get || type -P yum)
+[[ ! $cmd ]] && err "此脚本仅支持 ${yellow}(Ubuntu or Debian or CentOS)${none}."
+
+# systemd
+[[ ! $(type -P systemctl) ]] && {
+    err "此系统缺少 ${yellow}(systemctl)${none}, 请尝试执行:${yellow} ${cmd} update -y;${cmd} install systemd -y ${none}来修复此错误."
+}
+
+# wget installed or none
+is_wget=$(type -P wget)
+
+# x64
+case $(uname -m) in
+amd64 | x86_64)
+    is_arch=amd64
+    ;;
+*aarch64* | *armv8*)
+    is_arch=arm64
+    ;;
+*)
+    err "此脚本仅支持 64 位系统..."
+    ;;
+esac
+
+is_core=sing-box
+is_core_name=sing-box
+is_core_dir=/etc/$is_core
+is_core_bin=$is_core_dir/bin/$is_core
+is_core_repo=SagerNet/$is_core
+is_conf_dir=$is_core_dir/conf
+is_log_dir=/var/log/$is_core
+is_sh_bin=/usr/local/bin/$is_core
+is_sh_dir=$is_core_dir/sh
+is_sh_repo=$author/CherryScript/main/proxy/sing_box
+is_pkg="wget tar"
+is_config_json=$is_core_dir/config.json
+tmp_var_lists=(
+    tmpcore
+    tmpsh
+    tmpjq
+    is_core_ok
+    is_sh_ok
+    is_jq_ok
+    is_pkg_ok
+)
+
+# tmp dir
+tmpdir=$(mktemp -u)
+[[ ! $tmpdir ]] && {
+    tmpdir=/tmp/tmp-$RANDOM
+}
+
+# set up var
+for i in ${tmp_var_lists[*]}; do
+    export $i=$tmpdir/$i
+done
+
+# load bash script.
+load() {
+    . $is_sh_dir/src/$1
+}
+
+# wget add --no-check-certificate
+_wget() {
+    [[ $proxy ]] && export https_proxy=$proxy
+    wget --no-check-certificate $*
+}
+
+# print a mesage
+msg() {
+    case $1 in
+    warn)
+        local color=$yellow
+        ;;
+    err)
+        local color=$red
+        ;;
+    ok)
+        local color=$green
+        ;;
+    esac
+
+    echo -e "${color}$(date +'%T')${none}) ${2}"
+}
+
+
+# install dependent pkg
+install_pkg() {
+    cmd_not_found=
+    for i in $*; do
+        [[ ! $(type -P $i) ]] && cmd_not_found="$cmd_not_found,$i"
+    done
+    if [[ $cmd_not_found ]]; then
+        pkg=$(echo $cmd_not_found | sed 's/,/ /g')
+        msg warn "安装依赖包 >${pkg}"
+        $cmd install -y $pkg &>/dev/null
+        if [[ $? != 0 ]]; then
+            [[ $cmd =~ yum ]] && yum install epel-release -y &>/dev/null
+            $cmd update -y &>/dev/null
+            $cmd install -y $pkg &>/dev/null
+            [[ $? == 0 ]] && >$is_pkg_ok
+        else
+            >$is_pkg_ok
+        fi
+    else
+        >$is_pkg_ok
+    fi
+}
+
+# download file
+download() {
+    case $1 in
+    core)
+        [[ ! $is_core_ver ]] && is_core_ver=$(_wget -qO- "https://api.github.com/repos/${is_core_repo}/releases/latest?v=$RANDOM" | grep tag_name | egrep -o 'v([0-9.]+)')
+        [[ $is_core_ver ]] && link="https://github.com/${is_core_repo}/releases/download/${is_core_ver}/${is_core}-${is_core_ver:1}-linux-${is_arch}.tar.gz"
+        name=$is_core_name
+        tmpfile=$tmpcore
+        is_ok=$is_core_ok
+        ;;
+    sh)
+        link=https://raw.githubusercontent.com/railzen/CherryScript/main/ludo.sh
+        mkdir -p /etc/sing-box
+        mkdir -p /etc/sing-box/sh
+        cd /etc/sing-box/sh
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/bbr.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/caddy.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/core.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/dns.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/download.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/help.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/import.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/init.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/systemd.sh"
+        curl -s "https://raw.githubusercontent.com/railzen/CherryScript/main/proxy/sing-box.sh"
+        chmod +x *.sh
+        cp sing-box.sh ..
+        cd -
+        name="$is_core_name 脚本"
+        tmpfile=$tmpsh
+        is_ok=$is_sh_ok
+        ;;
+    jq)
+        link=https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-$is_arch
+        name="jq"
+        tmpfile=$tmpjq
+        is_ok=$is_jq_ok
+        ;;
+    esac
+
+    [[ $link ]] && {
+        #msg warn "下载 ${name} > ${link}"
+        if _wget -t 3 -q -c $link -O $tmpfile; then
+            mv -f $tmpfile $is_ok
+        fi
+    }
+}
+
+# get server ip
+get_ip() {
+    export "$(_wget -4 -qO- https://one.one.one.one/cdn-cgi/trace | grep ip=)" &>/dev/null
+    [[ -z $ip ]] && export "$(_wget -6 -qO- https://one.one.one.one/cdn-cgi/trace | grep ip=)" &>/dev/null
+}
+
+# check background tasks status
+check_status() {
+    # dependent pkg install fail
+    [[ ! -f $is_pkg_ok ]] && {
+        msg err "安装依赖包失败"
+        msg err "请尝试手动安装依赖包: $cmd update -y; $cmd install -y $pkg"
+        is_fail=1
+    }
+
+    # download file status
+    if [[ $is_wget ]]; then
+        [[ ! -f $is_core_ok ]] && {
+            msg err "下载 ${is_core_name} 失败"
+            is_fail=1
+        }
+        [[ ! -f $is_sh_ok ]] && {
+            msg err "下载 ${is_core_name} 脚本失败"
+            is_fail=1
+        }
+        [[ ! -f $is_jq_ok ]] && {
+            msg err "下载 jq 失败"
+            is_fail=1
+        }
+    else
+        [[ ! $is_fail ]] && {
+            is_wget=1
+            [[ ! $is_core_file ]] && download core &
+            [[ ! $local_install ]] && download sh &
+            [[ $jq_not_found ]] && download jq &
+            get_ip
+            wait
+            check_status
+        }
+    fi
+
+    # found fail status, remove tmp dir and exit.
+    [[ $is_fail ]] && {
+        exit_and_del_tmpdir
+    }
+}
+
+
+# exit and remove tmpdir
+exit_and_del_tmpdir() {
+    rm -rf $tmpdir
+    [[ ! $1 ]] && {
+        msg err "哦豁.."
+        msg err "安装过程出现错误..."
+        echo -e "反馈问题) https://github.com/${is_sh_repo}/issues"
+        echo
+        exit 1
+    }
+    exit
+}
+
+# main
+main() {
+
+    # check old version
+    [[ -f $is_sh_bin && -d $is_core_dir/bin && -d $is_sh_dir && -d $is_conf_dir ]] && {
+        #err "检测到脚本已安装, 如需重装请使用${green} ${is_core} reinstall ${none}命令."
+        /etc/sing-box/sh/sing-box.sh
+        exit 0
+        
+    }
+
+    clear
+    # start installing...
+    [[ $is_core_ver ]] && msg warn "${is_core_name} 版本: ${yellow}$is_core_ver${none}"
+    [[ $proxy ]] && msg warn "使用代理: ${yellow}$proxy${none}"
+    # create tmpdir
+    mkdir -p $tmpdir
+    # if is_core_file, copy file
+    [[ $is_core_file ]] && {
+        cp -f $is_core_file $is_core_ok
+        msg warn "${yellow}${is_core_name} 文件使用 > $is_core_file${none}"
+    }
+    # local dir install sh script
+    [[ $local_install ]] && {
+        >$is_sh_ok
+        msg warn "${yellow}本地获取安装脚本 > $PWD ${none}"
+    }
+
+    timedatectl set-ntp true &>/dev/null
+    [[ $? != 0 ]] && {
+        is_ntp_on=1
+    }
+
+    # install dependent pkg
+    install_pkg $is_pkg &
+
+    # jq
+    if [[ $(type -P jq) ]]; then
+        >$is_jq_ok
+    else
+        jq_not_found=1
+    fi
+    # if wget installed. download core, sh, jq, get ip
+    [[ $is_wget ]] && {
+        [[ ! $is_core_file ]] && download core &
+        [[ ! $local_install ]] && download sh &
+        [[ $jq_not_found ]] && download jq &
+        get_ip
+    }
+
+    # waiting for background tasks is done
+    wait
+
+    # check background tasks status
+    check_status
+
+    # test $is_core_file
+    if [[ $is_core_file ]]; then
+        mkdir -p $tmpdir/testzip
+        tar zxf $is_core_ok --strip-components 1 -C $tmpdir/testzip &>/dev/null
+        [[ $? != 0 ]] && {
+            msg err "${is_core_name} 文件无法通过测试."
+            exit_and_del_tmpdir
+        }
+        [[ ! -f $tmpdir/testzip/$is_core ]] && {
+            msg err "${is_core_name} 文件无法通过测试."
+            exit_and_del_tmpdir
+        }
+    fi
+
+    # create sh dir...
+    mkdir -p $is_sh_dir
+
+    # copy sh file or unzip sh zip file.
+    if [[ $local_install ]]; then
+        cp -rf $PWD/* $is_sh_dir
+    else
+        tar zxf $is_sh_ok -C $is_sh_dir
+    fi
+
+    # create core bin dir
+    mkdir -p $is_core_dir/bin
+    # copy core file or unzip core zip file
+    if [[ $is_core_file ]]; then
+        cp -rf $tmpdir/testzip/* $is_core_dir/bin
+    else
+        tar zxf $is_core_ok --strip-components 1 -C $is_core_dir/bin
+    fi
+
+    # add alias
+    echo "alias sb=$is_sh_bin" >>/root/.bashrc
+    echo "alias $is_core=$is_sh_bin" >>/root/.bashrc
+
+    # core command
+    ln -sf $is_sh_dir/$is_core.sh $is_sh_bin
+    ln -sf $is_sh_dir/$is_core.sh ${is_sh_bin/$is_core/sb}
+
+    # jq
+    [[ $jq_not_found ]] && mv -f $is_jq_ok /usr/bin/jq
+
+    # chmod
+    chmod +x $is_core_bin $is_sh_bin /usr/bin/jq ${is_sh_bin/$is_core/sb}
+
+    # create log dir
+    mkdir -p $is_log_dir
+
+
+    # create systemd service
+    load systemd.sh
+    install_service $is_core &>/dev/null
+
+    # create condf dir
+    mkdir -p $is_conf_dir
+
+
+   # remove tmp dir and exit.
+    /etc/sing-box/sh/sing-box.sh
+    
+    exit_and_del_tmpdir ok
+}
+
+# start.
+main $@
